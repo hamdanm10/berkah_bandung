cb477b5a9b1342826e6ef5900c4296435fc1f8e8
hamdanm10 <midunhamdan@gmail.com>
2026-01-16 19:16:46 +0700

    adding sidebar component

---
+++ app/components/ui/sidebar_link_component.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Ui::SidebarLinkComponent < ViewComponent::Base
+  include LucideRails::RailsHelper
+
+  attr_reader :icon, :url, :name
+
+  def initialize(icon:, url:, name: nil)
+    @icon = icon
+    @url = url
+    @name = name
+    @link_classes = link_classes
+  end
+
+  def before_render
+    @active = is_active?
+  end
+
+  private
+
+  def is_active?
+    helpers.request.path.include?(url)
+  end
+
+  def link_classes
+    case @active
+    when true
+      "text-primary bg-primary-soft"
+    when false
+      "text-secondary hover:bg-secondary-soft"
+    end
+  end
+end
