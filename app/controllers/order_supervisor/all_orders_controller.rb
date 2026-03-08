47a4cde2e7fab688ea22029c954828c1e4d3d7f1
hamdanm10 <midunhamdan@gmail.com>
2026-03-08 14:57:10 +0700

    update all

---
+++ app/controllers/order_supervisor/all_orders_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class OrderSupervisor::AllOrdersController < OrderSupervisorApplicationController
+  def index
+    limit = RecordLimit.call(params[:limit])
+
+    @q = Order.where(deleted_at: nil).ransack(params[:q])
+    @orders = @q
+              .result
+              .with_total_items
+              .includes(:courier_service, order_batch: %i[user_created merchant])
+              .order(created_at: :desc)
+    @pagy, @orders = pagy(@orders, limit:)
+  end
+
+  def show
+    @order = order_scope
+  end
+
+  private
+
+  def order_scope
+    Order
+      .with_total_items
+      .includes(:courier_service, order_batch: %i[user_created merchant], order_items: :product)
+      .find_by!(
+        id: params[:id],
+        deleted_at: nil
+      )
+  end
+end
