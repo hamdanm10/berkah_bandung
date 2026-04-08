0d45de7f6846592f5ddf930598ecf6a75b6f193e
hamdanm10 <midunhamdan@gmail.com>
2026-04-08 12:46:48 +0700

    update all

---
+++ app/controllers/warehouse_admin/categories_controller.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+class WarehouseAdmin::CategoriesController < WarehouseAdminApplicationController
+  def index
+    limit = RecordLimit.call(params[:limit])
+
+    @q = Category.where(deleted_at: nil).ransack(params[:q])
+    @categories = @q.result.order(created_at: :desc)
+    @pagy, @categories = pagy(@categories, limit:)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    result = Categories::Create.call(
+      category_params: category_params
+    )
+
+    if result.success?
+      redirect_to new_warehouse_admin_category_path, notice: result.payload[:message]
+    else
+      @category = result.error[:category]
+
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = category_scope
+  end
+
+  def update
+    result = Categories::Update.call(
+      category: category_scope,
+      category_params: category_params
+    )
+
+    if result.success?
+      category = result.payload[:category]
+
+      redirect_to edit_warehouse_admin_category_path(category), notice: result.payload[:message]
+    else
+      @category = result.error[:category]
+
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    result = Categories::SoftDelete.call(
+      category: category_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_categories_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_categories_path, alert: result.error[:category]
+    end
+  end
+
+  def activate
+    result = Categories::Activate.call(
+      category: category_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_categories_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_categories_path, alert: result.error[:category]
+    end
+  end
+
+  def deactivate
+    result = Categories::Deactivate.call(
+      category: category_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_categories_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_categories_path, alert: result.error[:category]
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def category_scope
+    Category.find(params[:id])
+  end
+end
