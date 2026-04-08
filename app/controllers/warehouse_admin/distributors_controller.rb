adfd3204d5d96360205930b415cc5d46eee069f5
hamdanm10 <midunhamdan@gmail.com>
2026-04-08 12:37:14 +0700

    update all

---
+++ app/controllers/warehouse_admin/distributors_controller.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+class WarehouseAdmin::DistributorsController < WarehouseAdminApplicationController
+  def index
+    limit = RecordLimit.call(params[:limit])
+
+    @q = Distributor.where(deleted_at: nil).ransack(params[:q])
+    @distributors = @q.result.order(created_at: :desc)
+    @pagy, @distributors = pagy(@distributors, limit:)
+  end
+
+  def new
+    @distributor = Distributor.new
+  end
+
+  def create
+    result = Distributors::Create.call(
+      distributor_params: distributor_params
+    )
+
+    if result.success?
+      redirect_to new_warehouse_admin_distributor_path, notice: result.payload[:message]
+    else
+      @distributor = result.error[:distributor]
+
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @distributor = show_distributor_scope
+  end
+
+  def edit
+    @distributor = distributor_scope
+  end
+
+  def update
+    result = Distributors::Update.call(
+      distributor: distributor_scope,
+      distributor_params: distributor_params
+    )
+
+    if result.success?
+      distributor = result.payload[:distributor]
+
+      redirect_to edit_warehouse_admin_distributor_path(distributor), notice: result.payload[:message]
+    else
+      @distributor = result.error[:distributor]
+
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    result = Distributors::Update.call(
+      distributor: distributor_scope,
+      distributor_params: distributor_params
+    )
+
+    if result.success?
+      distributor = result.payload[:distributor]
+
+      redirect_to edit_warehouse_admin_distributor_path(distributor), notice: result.payload[:message]
+    else
+      @distributor = result.error[:distributor]
+
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    result = Distributors::SoftDelete.call(
+      distributor: distributor_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_distributors_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_distributors_path, alert: result.error[:distributor]
+    end
+  end
+
+  def activate
+    result = Distributors::Activate.call(
+      distributor: distributor_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_distributors_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_distributors_path, alert: result.error[:distributor]
+    end
+  end
+
+  def deactivate
+    result = Distributors::Deactivate.call(
+      distributor: distributor_scope
+    )
+
+    if result.success?
+      redirect_to warehouse_admin_distributors_path, notice: result.payload[:message]
+    else
+      redirect_to warehouse_admin_distributors_path, alert: result.error[:distributor]
+    end
+  end
+
+  def search
+    q = params[:q].to_s.strip[0, 100]
+
+    distributors = Distributor
+                   .where(is_active: true, deleted_at: nil)
+                   .where('name ILIKE ?', "%#{q}%")
+                   .order(:name)
+                   .limit(15)
+
+    render json: distributors.map { |d|
+      { value: d.id, label: d.name }
+    }
+  end
+
+  private
+
+  def distributor_params
+    params.require(:distributor).permit(:name)
+  end
+
+  def distributor_scope
+    Distributor.find(params[:id])
+  end
+
+  def show_distributor_scope
+    Distributor.includes(:distributor_items).find(params[:id])
+  end
+end
